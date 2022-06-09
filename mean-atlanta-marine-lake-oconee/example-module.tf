d53727d0da2bf28a36cd753bd7159a17297fc9b9
Rajan Sharma <web.rajansharma@gmail.com>
2022-06-09 07:41:10 +0530

    update for all new files

---
+++ mean-atlanta-marine-lake-oconee/example-module.tf
@@ -0,0 +1,11 @@
+module "beanstalk-module" {
+  source              = "./beanstalk-module"
+  github_repository   = var.github_repository
+  github_branch       = var.github_branch
+  github_organization = var.github_organization
+  solution_stackname  = var.solution_stackname
+  region              = var.region
+  OAuthToken          = var.OAuthToken
+  root_domain         = var.root_domain
+  sub_domain         = var.sub_domain
+}
