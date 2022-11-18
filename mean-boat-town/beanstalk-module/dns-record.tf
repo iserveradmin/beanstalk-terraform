b921ef745ceb215e8b5cb91a2454241029148ae2
waleed <waleedkhalid.intern@vizzwebsolutions.com>
2022-11-18 17:48:41 +0500

    mean-boat-town

---
+++ mean-boat-town/beanstalk-module/dns-record.tf
@@ -0,0 +1,23 @@
+data "aws_elastic_beanstalk_hosted_zone" "current" {}
+resource "aws_route53_record" "dns_update" {
+  name    = aws_acm_certificate.static_url.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.root_domain.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_elastic_beanstalk_environment.beanstalk_app_env.cname
+    zone_id                = data.aws_elastic_beanstalk_hosted_zone.current.id
+  }
+}
+
+# Disabled below configuraiton to keep working on debug and unblock team
+
+# resource "aws_route53_record" "dns_update_cname" {
+#   for_each = { for k in aws_acm_certificate.static_url.subject_alternative_names : k => k if var.sub_domain == "" }
+#   name     = each.value
+#   type     = "CNAME"
+#   ttl      = "5"
+#   zone_id  = data.aws_route53_zone.root_domain.id
+#   records  = [aws_acm_certificate.static_url.domain_name]
+# }
